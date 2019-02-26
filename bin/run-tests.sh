a79bd25c8d9135c0c23f17e4995e6f70120ba417
Balakrishnan <balakrishnan.ramasamy@husqvarnagroup.com>
2019-02-26 11:16:13 +0100

    BD-1196 Add GETL load feature
    -Add function to load json with schema into dataframe
    -Add function to load xml with schema into dataframe
    -Add function to load textfile without schema into RDD
    -Add pytest cases and make sure 100% coverage
    -Add .gitignore
    -Add run-tests.sh for bamboo job
    -Add sample files in data folder for test purpose

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,4 +1,5 @@
-#!/bin/sh -e
+
+#!/bin/bash -e
 
 echo 'Starting tests'
 
@@ -13,11 +14,11 @@ rm -rf coverage-reports/* 2>/dev/null
 
 
 pipenv install --dev
-pipenv run pytest --cov big_data_getl --cov-report=xml:/coverage-reports/coverage.xml --junitxml=/test-reports/junit_coverage.xml --cov-report term
-sed -i "s#/home/ubuntu/big/big_data_getl#$BAMBOOWD/big_data_getl#g" /coverage-reports/coverage.xml >> /coverage-reports/coverage.xml
+pipenv run pytest --cov big_data_getl --cov-report=xml:./coverage-reports/coverage.xml --junitxml=./test-reports/junit_coverage.xml --cov-report term
+sed -i "s#/home/ubuntu/big/big_data_getl#$BAMBOOWD/big_data_getl#g" ./coverage-reports/coverage.xml >> ./coverage-reports/coverage.xml
 
 
 # Generate pylint report
-pipenv run pylint big_data_getl -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" | grep "^[a-zA-Z0-9]" >> ../test-reports/pylint-report.txt
-chmod 777 -R ../coverage-reports
-chmod 777 -R ../test-reports
+pipenv run pylint big_data_getl -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" | grep "^[a-zA-Z0-9]" >> ./test-reports/pylint-report.txt
+chmod 755 -R ./coverage-reports/
+chmod 755 -R ./test-reports/

d212ac145cdc7269cceaacf36c68b0d15e5384ec
Christian Ekstrom <christian.ekstrom@husqvarnagroup.com>
2019-03-06 10:24:55 +0100

    BD-1196 Try to fix bamboo working dir for coverage

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,14 @@
 """ConfTest fixture for SparkSession and logger."""
+import boto3
 import pytest
+from moto import mock_s3
+from typing import List
+
 from pyspark.sql import SparkSession
 
 
 @pytest.fixture(scope="module")
-def spark_session():
+def spark_session() -> SparkSession:
     """Return a sparksession fixture."""
     spark = (
         SparkSession
@@ -15,3 +19,11 @@ def spark_session():
     )
 
     yield spark
+
+
+@mock_s3
+@pytest.fixture(scope="function")
+def s3_mock() -> boto3.resource('s3'):
+    """Mock boto3 using moto library."""
+    mock_s3().start()
+    yield boto3.client('s3')
